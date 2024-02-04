72616b2f842506621eff2a7c6de36ad65346842b
Madis Pink <mail@madis.pink>
2024-02-04 19:12:08 +0200

    ci: add rudimentary CI actions

--- openapi/build.gradle.kts
+++ openapi/build.gradle.kts
@@ -3,4 +3,14 @@ plugins {
 }
 
 pkl {
+  evaluators {
+    val examples = listOf("apiWithExamples", "callbackExample", "linkExample", "petstore", "petstoreExpanded", "uspto")
+    examples.forEach { exampleName ->
+      register("example_${exampleName}") {
+        sourceModules.add(file("examples/${exampleName}.pkl"))
+        outputFile.set(layout.buildDirectory.file("generated/${exampleName}.yaml"))
+        outputFormat.set("yaml")
+      }
+    }
+  }
 }
