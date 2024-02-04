bb3f373b7ec40ada5aaf854b4a5ec15a7a63b0b1
Madis Pink <1147723+madisp@users.noreply.github.com>
2024-02-04 19:34:34 +0200

    ci: add package build and archive (#3)

--- openapi/build.gradle.kts
+++ openapi/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
   alias(libs.plugins.pkl)
 }
 
+val tag = providers.environmentVariable("GITHUB_TAG")
+
 pkl {
   evaluators {
     val examples = listOf("apiWithExamples", "callbackExample", "linkExample", "petstore", "petstoreExpanded", "uspto")
@@ -10,6 +12,15 @@ pkl {
         sourceModules.add(file("examples/${exampleName}.pkl"))
         outputFile.set(layout.buildDirectory.file("generated/${exampleName}.yaml"))
         outputFormat.set("yaml")
+      }
+    }
+  }
+
+  project {
+    packagers {
+      register("makePackages") {
+        projectDirectories.from(file("."))
+        environmentVariables = tag.map { mapOf("PKL_PUBLISH_VERSION" to it.removePrefix("v")) }.orElse(emptyMap())
       }
     }
   }
